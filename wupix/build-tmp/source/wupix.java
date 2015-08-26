d3b91739f0e2878eaae3adb765c2b80ddc47ea1f
tripzilch <pseudoscalar@gmail.com>
2015-08-26 14:28:08 +0200

    wupix now uses P2D/GL for simpler AA

--- wupix/build-tmp/source/wupix.java
+++ wupix/build-tmp/source/wupix.java
@@ -18,43 +18,107 @@ public class wupix extends PApplet {
 
 
 
-float w2;
+float pscale, dot_size;
+PShape box, hex;
 public void setup() {
-  size(500,500);
-  w2 = width / 2.0f;
+  size(1000,1000,P2D);
+  pscale = width / 2.0f;
+  dot_size = 2.0f / pscale;
+
+  smooth(4);
   colorMode(RGB, 1.0f);
   noStroke();
-  noiseSeed(23);
-  //translate(1, 1);
+
+  box = createShape();
+  box.beginShape(TRIANGLE_STRIP);
+  box.noStroke();
+  box.fill(0xff000000);
+  box.vertex(-0.707f * dot_size,  0.707f * dot_size);
+  box.vertex(-0.707f * dot_size, -0.707f * dot_size);
+  box.vertex( 0.707f * dot_size,  0.707f * dot_size);
+  box.vertex( 0.707f * dot_size, -0.707f * dot_size);
+  box.endShape(CLOSE);
+
+  hex = createShape();
+  hex.beginShape(TRIANGLE_STRIP);
+  hex.noStroke();
+  hex.fill(0xff000000);
+  hex.vertex(-0.866f * dot_size,  0.5f * dot_size);
+  hex.vertex(-0.866f * dot_size, -0.5f * dot_size);
+  hex.vertex( 0.000f * dot_size,  1.0f * dot_size);
+  hex.vertex( 0.000f * dot_size, -1.0f * dot_size);
+  hex.vertex( 0.866f * dot_size,  0.5f * dot_size);
+  hex.vertex( 0.866f * dot_size, -0.5f * dot_size);
+  hex.endShape(CLOSE);
+
+  box.disableStyle();
+  hex.disableStyle();
+
+  fill(1.0f,.7f,.3f);
+  rect(0, 0, width, height);
 }
 
-public void wupix(float x, float y) {
-  x = (x + 1.0f) * width / 2;
-  y = (y + 1.0f) * width / 2;
-  int xi = (int) x, yi = (int) y;
-  if (xi >= 0 && xi < width && yi >= 0 && yi < height) {
+public void wupix(double x, double y) {
+  /*int xi = (int) x, yi = (int) y;
+  if (xi >= 0 && xi < width - 1 && yi >= 0 && yi < height - 1) {
     int ii = yi * width + xi;
-    float xf = (x - xi), yf = (y - yi);
-    float xy = xf * yf;
-    pixels[ii] = color(1 - yf - xf + xy);
-    pixels[ii+1] = color(xf - xy);
-    pixels[ii+width] = color(yf - xy);
-    pixels[ii+width+1] = color(xy);
-  }
+    double xf = (x - xi), yf = (y - yi);
+    double xy = xf * yf;
+    pixels[ii] = grey(1.0 - yf - xf + xy);
+    pixels[ii+1] = grey(xf - xy);
+    pixels[ii+width] = grey(yf - xy);
+    pixels[ii+width+1] = grey(xy);
+  }*/
 }
 
-float start_time = millis();
-public void draw() {
-  float now = (millis() - start_time) / 1000.0f;
-  fill(0);//,0,0,0.2);
-  rect(0,0,width,height);
-  loadPixels();
-  for (int i = 0; i < 100; i++) {
-    float x = 2 * noise(i * 3.8f - now * .035f, 1 + now * .029f, now * .033f) - 1;
-    float y = 2 * noise(i * 7.2f + now * .028f, 2 - now * .034f, now * .030f) - 1;
-    wupix(x, y);
+public double wobble(double x, double y, double z) {
+  double a = Math.cos(23 * x - 17 * y + 77);
+  double b = Math.cos(19 * y - 22 * z + 55);
+  double c = Math.cos(27 * z - 13 * x + 33);
+
+  return  (Math.cos(7 * a - 8 * b + 39)
+         + Math.cos(9 * b - 6 * c + 24)
+         + Math.cos(7 * c - 5 * a + 15)) * 0.33333f;
+}
+
+public void dot(double x, double y, double s) {
+  float ps = dot_size * (float) s;
+  shape(hex, (float) x, (float) y, ps, ps);
+}
+
+public void flurp(double fi, double t) {
+  double x = wobble(fi * 38 - t * 35, 1 + t * 29, t * 33);
+  double y = wobble(fi * 72 + t * 28, 2 - t * 34, t * 30);
+  double s = 30.0f * Math.pow(wobble(fi * 55 + t * 27, 5 - t * 29, t * 53), 9.0f);
+  if (s > 0) {
+    fill(0.0f, 0.0f, 0.2f, (float) Math.pow(Math.min(1.0f, s), 2.2f));
+  } else {
+    s = Math.abs(s);
+    fill(1.0f, 0.9f, 0.8f, (float) Math.pow(Math.min(1.0f, s), 2.2f));
   }
-  updatePixels();
+  dot(x, y, Math.max(1.0f, s));
+}
+
+double start_time = millis();
+double prev_now = 0.0f;
+public void draw() {
+  double now = (float) frameCount / 12.0f; //(millis() - start_time) / 1000.0;
+
+  //fill(.9,.8,.3, 0.1);
+  //rect(0, 0, width, height);
+  scale(pscale); translate(1, 1);
+  int N = 8;
+  for (int i = 0; i < N; i++) {
+    double fi = (float) i / (float) N;
+    double t;
+    t = (0.2f * now + 0.8f * prev_now) * 3e-5f; flurp(fi, t);
+    t = (0.4f * now + 0.6f * prev_now) * 3e-5f; flurp(fi, t);
+    t = (0.6f * now + 0.4f * prev_now) * 3e-5f; flurp(fi, t);
+    t = (0.8f * now + 0.2f * prev_now) * 3e-5f; flurp(fi, t);
+    t = (1.0f * now + 0.0f * prev_now) * 3e-5f; flurp(fi, t);
+  }
+
+  prev_now = now;
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "wupix" };
--- wupix/wupix.pde
+++ wupix/wupix.pde
@@ -1,40 +1,104 @@
 import java.lang.Math;
 
-float w2;
+float pscale, dot_size;
+PShape box, hex;
 void setup() {
-  size(500,500);
-  w2 = width / 2.0;
+  size(1000,1000,P2D);
+  pscale = width / 2.0;
+  dot_size = 2.0 / pscale;
+
+  smooth(4);
   colorMode(RGB, 1.0);
   noStroke();
-  noiseSeed(23);
-  //translate(1, 1);
+
+  box = createShape();
+  box.beginShape(TRIANGLE_STRIP);
+  box.noStroke();
+  box.fill(#000000);
+  box.vertex(-0.707 * dot_size,  0.707 * dot_size);
+  box.vertex(-0.707 * dot_size, -0.707 * dot_size);
+  box.vertex( 0.707 * dot_size,  0.707 * dot_size);
+  box.vertex( 0.707 * dot_size, -0.707 * dot_size);
+  box.endShape(CLOSE);
+
+  hex = createShape();
+  hex.beginShape(TRIANGLE_STRIP);
+  hex.noStroke();
+  hex.fill(#000000);
+  hex.vertex(-0.866 * dot_size,  0.5 * dot_size);
+  hex.vertex(-0.866 * dot_size, -0.5 * dot_size);
+  hex.vertex( 0.000 * dot_size,  1.0 * dot_size);
+  hex.vertex( 0.000 * dot_size, -1.0 * dot_size);
+  hex.vertex( 0.866 * dot_size,  0.5 * dot_size);
+  hex.vertex( 0.866 * dot_size, -0.5 * dot_size);
+  hex.endShape(CLOSE);
+
+  box.disableStyle();
+  hex.disableStyle();
+
+  fill(1.0,.7,.3);
+  rect(0, 0, width, height);
 }
 
-void wupix(float x, float y) {
-  x = (x + 1.0) * width / 2;
-  y = (y + 1.0) * width / 2;
-  int xi = (int) x, yi = (int) y;
-  if (xi >= 0 && xi < width && yi >= 0 && yi < height) {
+void wupix(double x, double y) {
+  /*int xi = (int) x, yi = (int) y;
+  if (xi >= 0 && xi < width - 1 && yi >= 0 && yi < height - 1) {
     int ii = yi * width + xi;
-    float xf = (x - xi), yf = (y - yi);
-    float xy = xf * yf;
-    pixels[ii] = color(1 - yf - xf + xy);
-    pixels[ii+1] = color(xf - xy);
-    pixels[ii+width] = color(yf - xy);
-    pixels[ii+width+1] = color(xy);
-  }
+    double xf = (x - xi), yf = (y - yi);
+    double xy = xf * yf;
+    pixels[ii] = grey(1.0 - yf - xf + xy);
+    pixels[ii+1] = grey(xf - xy);
+    pixels[ii+width] = grey(yf - xy);
+    pixels[ii+width+1] = grey(xy);
+  }*/
 }
 
-float start_time = millis();
-void draw() {
-  float now = (millis() - start_time) / 1000.0;
-  fill(0);//,0,0,0.2);
-  rect(0,0,width,height);
-  loadPixels();
-  for (int i = 0; i < 100; i++) {
-    float x = 2 * noise(i * 3.8 - now * .035, 1 + now * .029, now * .033) - 1;
-    float y = 2 * noise(i * 7.2 + now * .028, 2 - now * .034, now * .030) - 1;
-    wupix(x, y);
+double wobble(double x, double y, double z) {
+  double a = Math.cos(23 * x - 17 * y + 77);
+  double b = Math.cos(19 * y - 22 * z + 55);
+  double c = Math.cos(27 * z - 13 * x + 33);
+
+  return  (Math.cos(7 * a - 8 * b + 39)
+         + Math.cos(9 * b - 6 * c + 24)
+         + Math.cos(7 * c - 5 * a + 15)) * 0.33333;
+}
+
+void dot(double x, double y, double s) {
+  float ps = dot_size * (float) s;
+  shape(hex, (float) x, (float) y, ps, ps);
+}
+
+void flurp(double fi, double t) {
+  double x = wobble(fi * 38 - t * 35, 1 + t * 29, t * 33);
+  double y = wobble(fi * 72 + t * 28, 2 - t * 34, t * 30);
+  double s = 30.0 * Math.pow(wobble(fi * 55 + t * 27, 5 - t * 29, t * 53), 9.0);
+  if (s > 0) {
+    fill(0.0, 0.0, 0.2, (float) Math.pow(Math.min(1.0, s), 2.2));
+  } else {
+    s = Math.abs(s);
+    fill(1.0, 0.9, 0.8, (float) Math.pow(Math.min(1.0, s), 2.2));
   }
-  updatePixels();
+  dot(x, y, Math.max(1.0, s));
+}
+
+double start_time = millis();
+double prev_now = 0.0;
+void draw() {
+  double now = (float) frameCount / 12.0; //(millis() - start_time) / 1000.0;
+
+  //fill(.9,.8,.3, 0.1);
+  //rect(0, 0, width, height);
+  scale(pscale); translate(1, 1);
+  int N = 8;
+  for (int i = 0; i < N; i++) {
+    double fi = (float) i / (float) N;
+    double t;
+    t = (0.2 * now + 0.8 * prev_now) * 3e-5; flurp(fi, t);
+    t = (0.4 * now + 0.6 * prev_now) * 3e-5; flurp(fi, t);
+    t = (0.6 * now + 0.4 * prev_now) * 3e-5; flurp(fi, t);
+    t = (0.8 * now + 0.2 * prev_now) * 3e-5; flurp(fi, t);
+    t = (1.0 * now + 0.0 * prev_now) * 3e-5; flurp(fi, t);
+  }
+
+  prev_now = now;
 }
